83062ef7697f21b34ab517c6b41412d8448ba60f
marisolzhizhpon <marisolzhizhpon@localhost>
2022-03-30 20:47:00 -0500

    Convertir en español

---
+++ Poo-unidad01-zqmm/src/main/java/marisolzhizhpon/poo/unidad01/zqmm/Ejemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.poo.unidad01.zqmm;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Ejemplo {
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD");
+    }
+    
+}
