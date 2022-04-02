be3a6c8275f2536d2f245f68db41d3e3c27ab2fa
marisolzhizhpon <marisolzhizhpon@localhost>
2022-04-01 20:52:26 -0500

    Ejercicio

---
+++ Ejercicio_0101/src/main/java/marisolzhizhpon/ejercicio_0101/Carro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.ejercicio_0101;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Carro {
+    public String modelo;
+    public String año;
+    public String motor;
+    public String recorrido;
+    
+    
+    
+    
+}
