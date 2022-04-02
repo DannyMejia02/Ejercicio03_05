e200443c51594fb6c86dfa2f6d89ef98f0c33a4e
marisolzhizhpon <marisolzhizhpon@localhost>
2022-04-01 20:52:36 -0500

    Ejercicio

---
+++ Ejercicio_0101/src/main/java/marisolzhizhpon/ejercicio_0101/Producto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.ejercicio_0101;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Producto {
+    public String codigo;
+    public String calidad;
+    public String nombre;
+    public String marca;
+    
+}
