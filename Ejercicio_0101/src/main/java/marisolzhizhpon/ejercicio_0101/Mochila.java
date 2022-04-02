926b3c1f4eb7474ff6259915c138b03b6619276a
marisolzhizhpon <marisolzhizhpon@localhost>
2022-04-01 20:52:43 -0500

    Ejercicio

---
+++ Ejercicio_0101/src/main/java/marisolzhizhpon/ejercicio_0101/Mochila.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.ejercicio_0101;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Mochila {
+    public String precio;
+    public String color;
+    public String tamaño;
+    public String peso;
+    
+}
