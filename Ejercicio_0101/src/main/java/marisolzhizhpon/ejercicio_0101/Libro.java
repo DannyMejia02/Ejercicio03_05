199f05c9a1654fb69408de1df0d380f0b22ca746
marisolzhizhpon <marisolzhizhpon@localhost>
2022-04-01 20:52:21 -0500

    Ejercicio

---
+++ Ejercicio_0101/src/main/java/marisolzhizhpon/ejercicio_0101/Libro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.ejercicio_0101;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Libro {
+    public String titulo;
+    public String codigo;
+    public String autor;
+    public String idioma;
+    
+}
