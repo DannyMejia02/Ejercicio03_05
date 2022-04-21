d55adc3cb12796113f0658db2957e638ef6ada71
marisolzhizhpon <marisolzhizhpon@localhost>
2022-04-20 21:40:11 -0500

    Ejercicio1

---
+++ POO-Ejercicio01.03-G9/src/main/java/marisolzhizhpon/poo/ejercicio01/g9/Pelicula.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.poo.ejercicio01.g9;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Pelicula {
+    private String nombre;
+    private String tipoDePelicula;
+    private boolean duracion;
+    
+    public String nombrePelicula(){
+        return null;
+        
+    }
+    
+    public String tipoPelicula(){
+        return null;
+        
+    }
+    
+}
---
+++ POO-Ejercicio01.03-G9/src/main/java/marisolzhizhpon/poo/ejercicio01/g9/Principal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marisolzhizhpon.poo.ejercicio01.g9;
+
+/**
+ *
+ * @author marisolzhizhpon
+ */
+public class Principal {
+    
+}
